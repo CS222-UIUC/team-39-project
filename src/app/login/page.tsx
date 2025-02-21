a93c396c54fed69fdc1db75eed9b0065fbedbef8
zoryzhang <hereiszory@gmail.com>
2025-02-21 17:45:20 -0600

    simple homepage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,16 @@
 import Image from "next/image";
 
+function LoginButton() {
+    return (
+      <button>I'm a login button</button>
+    );
+}
+
 export default function Page() {
-    return <h1>This is our login page</h1>
+    return (
+        <div>
+            <h1>This is our login page</h1>
+            <LoginButton />
+        </div>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,43 @@
-//import { getPosts } from '@/lib/posts'
-//import { Post } from '@/ui/post'
- 
-//export default async function Page() {
-//  const posts = await getPosts()
- 
-//  return (
-//    <ul>
-//      {posts.map((post) => (
-//        <Post key={post.id} post={post} />
-//      ))}
-//    </ul>
-//  )
-//}
+// from https://react.dev/learn
+'use client'
+import { useState } from 'react';
+
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+};
 
 export default function Page() {
-    return <h1>Hello Next.js!</h1>
-  }
+    const [isLoggedIn, setIsLoggedIn] = useState(1);
+    function LogoutButton() {
+        function handleClick() {
+            setIsLoggedIn(0);
+            alert('You have logged out!');
+        }
+          
+        return (
+          <button onClick={handleClick}>Logout</button>
+        );
+    }
+    let content = <h1>Please login, {user.name}!</h1>;
+    if (isLoggedIn) {
+      content = 
+        <div>
+            <h1>Welcome back, {user.name}!</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                width: user.imageSize,
+                height: user.imageSize
+                }}
+            />
+            <LogoutButton />
+        </div>;
+    } else {
+        content = <h1>Please login, {user.name}!</h1>;
+    }
+    return <div> {content} </div>
+}
