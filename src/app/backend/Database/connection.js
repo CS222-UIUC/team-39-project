19748b2926df7fa05b8bf1cd3e49b2d085a106d5
MingqianW <larrywang0227@foxmail.com>
2025-03-05 17:57:04 -0600

    add comments for connection

--- src/app/backend/Database/connection.js
+++ src/app/backend/Database/connection.js
@@ -2,11 +2,13 @@ const mysql = require('mysql');
 
 
 const connection = mysql.createConnection({
-  host: 'localhost', 
-  user: 'username',
-  password: 'password',
-  database: 'databasename'
+  host: '127.', 
+  user: 'cs222',
+  password: '',
+  database: 'cs222'
 });
+
+// For now, the database can only be accessed from the local machine, so how can we make it accessible from other machine.
 
 // Connect to the database
 connection.connect((err) => {
