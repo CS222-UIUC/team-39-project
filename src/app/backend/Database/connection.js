6a3db9258998a81efbb4c679cee4506118b134f1
MingqianW <larrywang0227@foxmail.com>
2025-03-09 16:18:33 -0500

    database set-up and simple tests

--- src/app/backend/Database/connection.js
+++ src/app/backend/Database/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 
-
+//mysql -u admin -p -h cs222.cxcwkyiweck5.us-east-2.rds.amazonaws.com -P 3306
 const connection = mysql.createConnection({
-  host: '127.', 
-  user: 'cs222',
-  password: '',
-  database: 'cs222'
+  host: 'cs222.cxcwkyiweck5.us-east-2.rds.amazonaws.com', 
+  user: 'admin',
+  password: 'cs222database',
+  database: 'db',
+  port: 3306
 });
 
 // For now, the database can only be accessed from the local machine, so how can we make it accessible from other machine.
@@ -19,3 +20,4 @@ connection.connect((err) => {
   console.log('Connected to the database as ID ' + connection.threadId);
 });
 
+module.exports = connection;
