08fa9a509e0180926c2b2ac468b1ae3d7e020218
Sissi <Sissi_2004@outlook.com>
2025-03-14 17:09:32 -0500

    user/recipe controller and routes
    user/recipe controller and routes

--- src/app/backend/server.js
+++ src/app/backend/server.js
@@ -1,22 +1,34 @@
-const express = require('express');
 require('dotenv').config();
-
-const app = express();
-app.use(express.json());
-
-const port = process.env.PORT;
-
-const connection = require('./Database/connection');
+const express = require('express');
+const mongoose = require('mongoose')
 
 // Import routes
-const recipeRoute = require('./Routes/recipeRoute');
+const recipeRoute = require('./Routes/recipe');
+const userRoute = require('./Routes/user');
+const connection = require('./Database/connection');
 
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+  })
+
+const port = process.env.PORT || 3000; //?
+
+// routes
 app.use('/api/recipe', recipeRoute);
+app.use('/api/user', userRoute);
 
 app.get('/', (req, res) => {
     res.send('Backend is working!');
 });
 
-app.listen(port, '0.0.0.0', () => {
+//app.listen(port, '0.0.0.0', () => {
+  //  console.log(`Server running on http://localhost:${port}`);
+//});
+app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
-});
+  });
