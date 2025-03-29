d26228740c46227db77b16146e9fa499e09523bd
zoryzhang <hereiszory@gmail.com>
2025-03-29 10:07:31 -0500

    split login and signup

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+// from https://react.dev/learn
+import { SignupForm } from '@/app/ui/signup-form'
+
+//https://react.dev/reference/react-dom/components/input
+export default function Page() {
+    return (
+        <div>
+            <h1>Please sign-up</h1>
+            <SignupForm />
+        </div>
+    );
+}
--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -1,26 +1,21 @@
-//'use client'
-import { signup } from '@/app/actions/auth'
-//import { useActionState } from 'react'
+import { login } from '@/app/actions/auth'
  
-export function SignupForm() {
+export function LoginForm() {
     //const [state, action, pending] = useActionState(signup, undefined)
     return (
-        <form action={signup}>
+        <form action={login}>
         {/*<form action={action}>*/}
             <div>
                 <label htmlFor="username">User name: </label>
                 <input id="username" name="username" placeholder="at least 2 characters" />
-                {/*{state?.errors?.username && <p>{state.errors.username}</p>}*/}
             </div>
             
             <div>
                 <label htmlFor="password">Password: </label>
                 <input id="password" name="password" placeholder="at least 4 characters" />
-                {/*{state?.errors?.password && <p>{state.errors.password}</p>}*/}
             </div>
             
-            <button type="submit">Sign-up</button>
-            {/*<button disabled={pending} type="submit">Login/Sign-up</button>*/}
+            <button type="submit">Login</button>
         </form>
     );
 }
