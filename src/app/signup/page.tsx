85a91344d7f56e1268157747330a2f9f12d4734a
zoryzhang <hereiszory@gmail.com>
2025-04-21 18:27:00 -0500

    also update signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,6 +2,7 @@
 "use client";
 import { SignupForm } from '@/app/ui/signup-form'
 import { redirect } from 'next/navigation'
+import ReactiveButton from 'reactive-button';
 
 //https://react.dev/reference/react-dom/components/input
 export default function Page() {
@@ -10,14 +11,16 @@ export default function Page() {
             redirect('/login')
         }
         return (
-          <button onClick={handleClick}>Click here for logging in instead</button>
+            <ReactiveButton onClick={handleClick} color="violet" idleText="Click here for logging in instead" />
         );
     }
     return (
-        <div>
-            <h1>Please sign-up</h1>
-            <LoginPageButton />
-            <SignupForm />
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-[10px] row-start-2 items-center sm:items-start">
+                <h1 className="text-4xl font-semibold text-center">Please sign up</h1>
+                <LoginPageButton />
+                <SignupForm />
+            </main>
         </div>
     );
 }
--- src/app/ui/signup-form.tsx
+++ src/app/ui/signup-form.tsx
@@ -22,28 +22,30 @@ export function SignupForm() {
             }
             else setState('success');
         }}>
-            <div>
-                <label htmlFor="username">Username: </label>
-                <input id="username" name="username" placeholder="at least 2 characters" required minLength={2} />
-            </div>
-            
-            <div>
-                <label htmlFor="password">Password: </label>
-                <input id="password" name="password" type="password" placeholder="at least 4 characters" required minLength={4} />
-            </div>
-            
-            {error && <p style={{ color: 'red' }}>{error}</p>} {/* Display error message */}
-            
-            <ReactiveButton 
-                buttonState={state}
-                type="submit" 
-                color="violet" 
-                idleText="Sign up" 
-                loadingText="Wait up to 1min"
-                successText="Redirecting"
-                errorText="Try again"
-                messageDuration={3000}
-            />
+            <main className="flex flex-col gap-[5px] items-center">
+                <div>
+                    <label className="text-xl" htmlFor="username">Username: </label>
+                    <input className="text-lg" id="username" name="username" placeholder="at least 2 characters" required minLength={2} />
+                </div>
+                
+                <div>
+                    <label className="text-xl" htmlFor="password">Password: </label>
+                    <input className="text-lg" id="password" name="password" type="password" placeholder="at least 4 characters" required minLength={4} />
+                </div>
+                
+                {error && <p style={{ color: 'red' }}>{error}</p>} {/* Display error message */}
+                
+                <ReactiveButton 
+                    buttonState={state}
+                    type="submit" 
+                    color="violet" 
+                    idleText="Sign up" 
+                    loadingText="Wait up to 1min"
+                    successText="Redirecting"
+                    errorText="Try again"
+                    messageDuration={3000}
+                />
+            </main>
         </form>
     );
 }
